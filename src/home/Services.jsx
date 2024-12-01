1613184abf27f33bc8ad476b56711bc5b0e5f5cd
Danilo <pelasod@gmail.com>
2024-12-01 13:35:49 +0800

    major revision number 2

--- src/home/Services.jsx
+++ src/home/Services.jsx
@@ -56,22 +56,7 @@ function Services() {
                            Crafting intuitive and visually appealing user interfaces that align with the brand identity and engage users. Employing the latest front-end technologies, such as <span className="font-bold"> React </span>and<span className="font-bold"> Tailwind CSS</span> to create dynamic and interactive UI components that elevate the overall user experience.
                         </p>
                     </div>
-                    <div data-aos="fade-up" data-aos-offset="300" data-aos-duration="1000"
-                        className="col-span-1 flex flex-col gap-[14px]  backdrop-blur-sm border border-[#222222] p-5 bg-[#141414]/[20%] rounded-lg">
-                        <div className="flex items-center gap-3 ">
-                            <div className="w-[52px] h-[52px] bg-[#46025e] flex justify-center items-center rounded-full">
-                                <svg width="23" height="17" viewBox="0 0 23 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M16.75 3.64662L22 8.53455L16.75 13.4225M6.25 13.4225L1 8.53455L6.25 3.64662M13.75 0.853516L9.25 16.2156"
-                                        stroke="#FF17E8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                </svg>
-                            </div>
-                            <p className="text-white font-bold">Accessibility <br />
-                                Integration</p>
-                        </div>
-                        <p className="text-[#C1C1C1] w-full">
-                              Ensuring inclusivity by implementing web accessibility standards (WCAG) to make digital experiences accessible to all users, including those with disabilities. 
-                        </p>
-                    </div>
+                    
                 </div>
             </section>
 
